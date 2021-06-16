14a6bf66e6eea8236750366a8b8282d8bec5dabf
Fabio Pontes <faabiopontes@gmail.com>
2021-06-15 23:13:57 -0300

    chores: margin LabelList

--- src/components/LabelsList/styles.ts
+++ src/components/LabelsList/styles.ts
@@ -4,11 +4,14 @@ interface LabelProps {
   color: string;
 }
 
-export const Container = styled.ul``;
+export const Container = styled.div`
+  margin-top: 10px;
+`;
 
 export const Label = styled.span<LabelProps>`
   border-radius: 24px;
   padding: 2px 7px;
+  margin: 0px 1px;
   display: inline-block;
   background-color: #555;
   font-size: 14px;
