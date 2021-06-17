4f280db93058f354364368ac741e74ea91472e2f
Fabio Pontes <faabiopontes@gmail.com>
2021-06-17 05:57:03 -0300

    feat: Tests for GitHubAutocomplete search

--- src/components/GitHubAutocomplete/tests.spec.tsx
+++ src/components/GitHubAutocomplete/tests.spec.tsx
@@ -1,11 +1,30 @@
-import { render } from "@testing-library/react";
-import GitHubAutocomplete from './index';
+import { fireEvent, render, waitFor } from "@testing-library/react";
+import GitHubAutocomplete from "./index";
+import { mockIssues } from "../IssuesList/mocks";
+
+jest.mock("../../services/github", () => ({
+  searchIssuesByText: () => mockIssues,
+}));
 
 describe("GitHubAutocomplete", () => {
   it("should be rendered", () => {
     const { getByPlaceholderText } = render(<GitHubAutocomplete />);
-    const inputElement = getByPlaceholderText('Type here to get React issues');
+    const inputElement = getByPlaceholderText("Type here to get React issues");
 
     expect(inputElement).toBeDefined();
+  });
+
+  it("should search and render issues", async () => {
+    const { getByPlaceholderText, getByText } = render(<GitHubAutocomplete />);
+    const inputElement = getByPlaceholderText("Type here to get React issues");
+
+    fireEvent.change(inputElement, {
+      target: { value: "input value" },
+    });
+
+    await waitFor(() => {
+      const issueElement = getByText(mockIssues[0].title);
+      expect(issueElement).toBeDefined();
+    });
   });
 });
